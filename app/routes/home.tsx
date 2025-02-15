84b78cc4a8c55cfcae4c48b6df18080e2d185008
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-15 10:51:14 +0700

    Add files via upload

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import Left from "~/components/left";
 
 export function meta() {
   return [
-    { title: "Home | Fahmi XD" },
+    { title: "Home | Yogi Sugalih" },
     { name: "description", content: "Hai, namaku Fahmi, saya mempunyai hobi yaitu bermain game dan coding. Saya tertarik dalam dunia teknologi dan IT. Saya tinggal di kota Bandung, Indonesia, dan masih duduk di bangku SMK." },
   ];
 }
