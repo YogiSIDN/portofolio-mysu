45ec54bc324fe11f5bd6eb50913e6a0ac72b5638
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-15 09:35:00 +0700

    Changes commit

--- app/components/left.tsx
+++ app/components/left.tsx
@@ -10,7 +10,7 @@ export default function Left() {
           className="h-60 w-full bg-banner"
         >
           <video className="w-full h-[200px] object-cover" loop autoPlay muted>
-            <source src="https://files.catbox.moe/su17ug.mp4" />
+            <source src="https://files.catbox.moe/kpwbbj.mp4" />
             Maaf tampaknya browser anda tidak support tag video
           </video>
         </div>
