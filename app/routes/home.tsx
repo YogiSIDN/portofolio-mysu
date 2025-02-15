c05a83d3003aa5687be44d1b75221a7eb238abb8
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-15 11:03:35 +0700

    Add files via upload

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,7 @@ import Left from "~/components/left";
 export function meta() {
   return [
     { title: "Home | Yogi Sugalih" },
-    { name: "description", content: "Hai, namaku Fahmi, saya mempunyai hobi yaitu bermain game dan coding. Saya tertarik dalam dunia teknologi dan IT. Saya tinggal di kota Bandung, Indonesia, dan masih duduk di bangku SMK." },
+    { name: "description", content: "Hi, let me introduce myself. My name is Mohammad Yogi Sugalih, I am currently 23 years old, and I live in **Jakarta Barat**. My hobby is creating projects using JavaScript code. I enjoy exploring new technologies and building innovative solutions to solve real-world problems. Living in Jakarta Barat has given me access to a vibrant tech community, which has inspired me to grow and improve my skills. I am passionate about web development and always eager to learn more." },
   ];
 }
 
